9451d0521456c928f7abab3a8244c71d47ede7b5
David W <sluuckejoohn@gmail.com>
2024-06-10 01:17:21 -0300

    feat: mc-duck fake news

---
+++ LLM.py
@@ -0,0 +1,133 @@
+from abc import ABC, abstractmethod
+import requests
+from dotenv import load_dotenv
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import llm
+from langchain_community.document_loaders.csv_loader import CSVLoader
+import pandas as pd
+
+load_dotenv()
+# adapter
+
+
+# class llm:
+#     def prompt(self, message: str, llm: object) -> str:
+#         if isinstance(llm, ChatGPT):
+#             return llm.chat_prompt(message)
+#         else:
+#             return "No LLM found"
+
+
+# singleton
+class ChatGPT:
+    llm = None
+
+    def __init__(self):
+        if ChatGPT.llm is None:
+            __conn = ChatOpenAI(
+                temperature=1, model='gpt-4o')
+            ChatGPT.llm = __conn
+
+    def chat_prompt(self, prompt: object, data: str, afirmacao: str) -> str:
+        return llm.LLMChain(llm=ChatGPT.llm, prompt=prompt).run(
+            data=data, afirmacao=afirmacao)
+
+# Comportamental: Template Method
+
+
+class Parser(ABC):
+    def chat_prompt(self, query: str):
+        data = self.get_data(query)
+        self.generate_csv_data(data)
+        docs = self.parse_llm(query)
+        prompt = self.prompt_maker()
+
+        return {
+            'prompt': prompt,
+            'docs': docs,
+        }
+
+    @abstractmethod
+    def get_data():
+        pass
+
+    @abstractmethod
+    def generate_csv_data(self):
+        pass
+
+    @abstractmethod
+    def parse_llm(self):
+        pass
+
+    @abstractmethod
+    def prompt_maker(self):
+        pass
+
+
+class IBGEParser(Parser):
+    def __init__(self):
+        self.url = "http://servicodados.ibge.gov.br/api/v3/noticias/?q="
+
+    def get_data(self, query: str):
+        response = requests.get(self.url + query + "&qtd=1000")
+        data = response.json()
+        return data
+
+    def generate_csv_data(self, data: dict) -> map:
+        # data = self.get_data(query)
+
+        formattedData = map(
+            lambda x: {
+                "id": x['id'],
+                "title": x['titulo'],
+                "introducao": x['introducao'],
+                "link": x['link']
+            }, data['items'])
+
+        df = pd.DataFrame(formattedData)
+
+        df.to_csv('output.csv', index=False, encoding='utf8')
+
+    def parse_llm(self, query: str):
+        loader = CSVLoader(file_path='./output.csv', encoding='utf8')
+        docs = loader.load()
+        embeddings = OpenAIEmbeddings()
+
+        db = FAISS.from_documents(docs, embeddings)
+
+        similar = db.similarity_search(query=query, k=10)
+
+        return [doc.page_content for doc in similar]
+
+    def prompt_maker(self):
+        prompt_model = """
+        Você é um verificador de fake news, dado a base de dados abaixo, determine se a afirmação é verdadeira ou falsa.
+        Siga as instruções abaixo:
+        1/ Seja objetivo e claro
+        2/ Não use palavras ofensivas
+        3/ Justifique sua resposta com base na base de dados fornecida
+        4/ Coloque o link das notícias que você usou para justificar sua resposta
+        5/ Utilize alguns conectivos que são sons de pato para deixar a conversa mais divertida. Como por exemplo: "quack", "quack quack", "quack quack quack"
+        
+        Aqui está a base de dados:
+        {data}
+       
+        Aqui está a afirmação do usuário:
+        {afirmacao}
+        
+        
+        Responda com base neste modelo:
+        Olá, quack quack!
+        
+        
+        Escreva sua resposta: 
+        """
+
+        prompt = PromptTemplate(
+            input_variables=["data", "afirmacao"],
+            template=prompt_model
+        )
+
+        return prompt
--- flask_app.py
+++ flask_app.py
@@ -1,89 +1,109 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-import os
+from LLM import ChatGPT, IBGEParser, Parser
+import json
 
 app = Flask(__name__,
-static_url_path='/static',
-static_folder='public'
-)
+            static_url_path='/public',
+            static_folder='public'
+            )
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///minhabase.sqlite3"
 db = SQLAlchemy(app)
 UPLOAD_FOLDER = '/home/duckdavid/mysite/upload'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = '6537b410-df31-4078-aa58-78f6d90bdf8c'
 
-class Usuario(db.Model):
-    __tablename__ = "usuarios"
+
+class Users(db.Model):
+    __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String, unique=True)
-    senha = db.Column(db.String)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    searchs = db.relationship('Searchs', backref='users')
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
 
-    def __init__(self, nome, senha):
-        self.nome = nome
-        self.senha = senha
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
 
-def verificar_crendenciais(nome, senha):
-    user = Usuario.query.filter_by(nome = nome).first()
-    if(user and user.senha == senha):
+class Searchs(db.Model):
+    __tablename__ = "searchs"
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    question = db.Column(db.String)
+    answer = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+
+def verify(username, password):
+    user = Users.query.filter_by(username=username).first()
+    if (user and user.password == password):
         return True
 
     return False
 
-@app.route('/')
-def index(nome):
-   return render_template("index.html", visitante="David")
 
-
-@app.route("/usuario", methods=[ 'POST', 'GET'])
-def addUsuario():
-    if (request.method == "POST"):
-        nome = request.form['nome']
-        senha = request.form['senha']
-        user = Usuario(nome, senha)
-        db.session.add(user)
-        db.session.commit()
-    users = Usuario.query.all()
-    return render_template('usuario.html', usuarios=users)
-
-@app.route("/formulario", methods=['GET', 'POST'])
-def formulario():
-    if request.method == 'POST':
-        nome = request.form['nome']
-        senha = request.form['senha']
-        return render_template('formulario.html', nome=nome, senha=senha)
-    else:
-        return render_template('formulario.html')
-
-@app.route('/upload', methods=['POST', 'GET'])
-def upload():
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    is_logged = False
+    searchs = []
     if 'username' in session:
-        if request.method == 'POST':
-            file = request.files['arquivo']
-            savePath = os.path.join(UPLOAD_FOLDER, file.filename)
-            file.save(savePath)
-            return 'upload feito com sucesso'
-        return render_template('upload.html', username=session['username'])
-    return 'Você não está autorizado para ver essa página. Por favor, faça login'
+        is_logged = True
+        user = Users.query.filter_by(username=session['username']).first()
+        print(user.id)
+        searchs = Searchs.query.filter(Searchs.author_id == user.id).all()
 
-@app.route('/area_vip')
-def area_vip():
-    if 'username' in session:
-        return 'Você está na área vip, logado como {}'.format(session['username'])
-    return 'Você não está autorizado para ver essa página. Por favor, faça login'
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
     if request.method == 'POST':
-        nome =  request.form['username']
-        senha =  request.form['password']
-        if(verificar_crendenciais(nome, senha) == True):
+        username = request.form['username']
+        password = request.form['password']
+        if (verify(username, password) == True):
             session['username'] = request.form['username']
-            return redirect(url_for('upload'))
+            return redirect('/')
+        else:
+            user = Users(username, password)
+            db.session.add(user)
+            db.session.commit()
+            session['username'] = request.form['username']
 
-        return 'usuário ou senha errados, tente novamente'
-    return render_template('login.html')
+        return redirect('/')
+
+    return render_template("index.html", is_logged=is_logged, prompts=searchs)
+
+
+@app.route('/llm', methods=['POST'])
+def llm():
+    data: str = request.data
+
+    json_data = json.loads(data)
+    user_prompt = json_data['prompt']
+
+    if user_prompt == "":
+        return {'content': "Prompt is empty"}
+
+    chat = ChatGPT()
+    ibgeParser = IBGEParser()
+    prompts = ibgeParser.chat_prompt(user_prompt)
+    # ibgeParser.generate_csv_data(user_prompt)
+    # docs = ibgeParser.parse_llm(user_prompt)
+    # prompt = ibgeParser.prompt_maker()
+    content = chat.chat_prompt(
+        prompt=prompts['prompt'], docs=prompts['docs'], afirmacao=user_prompt)
+    if 'username' in session:
+        try:
+            user = Users.query.filter_by(username=session['username']).first()
+            new_search = Searchs(
+                author_id=user.id, question=user_prompt, answer=content)
+            db.session.add(new_search)
+            db.session.commit()
+        except:
+            print("Error on save search")
+            pass
+
+    return {
+        'content': content
+    }
 
 
 @app.route('/logout')
@@ -91,9 +111,9 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
+
 with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
     app.run()
-
